4beb62f83204061a0ed2a9d29beebeab2dbd03d7
NLGRF <non.devtech@gmail.com>
2019-04-06 17:26:29 +0700

    Update FP2019 ESP8266

--- ESP8266_Firebase_noLib/ESP8266_Firebase_noLib.ino
+++ ESP8266_Firebase_noLib/ESP8266_Firebase_noLib.ino
@@ -50,7 +50,7 @@ void loop() {
  **********************************************************/
 #ifndef FIREBASE_FINGERPRINT  // Fingerprint ของ https://www.firebaseio.com
 // ใช้ได้ตั้งแต่ 01/08/2018 17:21:49 GMT ถึง หมดอายุสิ้นสุด 27/03/2019 00:00:00 GMT
-#define FIREBASE_FINGERPRINT  "6F D0 9A 52 C0 E9 E4 CD A0 D3 02 A4 B7 A1 92 38 2D CA 2F 26"
+#define FIREBASE_FINGERPRINT "E2 34 53 7A 1E D9 7D B8 C5 02 36 0D B2 77 9E 5E 0F 32 71 17"
 #endif
 
 int TD_Set_Firebase(String path, String value , bool push) {
